67876dbb6712d135183c672e12d5f742d5c15a29
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-25 20:15:37 +0600

    add image slider below

--- src/Components/CarSearchSection/CarSearchSection.jsx
+++ src/Components/CarSearchSection/CarSearchSection.jsx
@@ -3,6 +3,9 @@ const CarSearchSection = () => {
   return (
     <div>
         <h1>Lets Find your car.</h1>
+        <div>
+            <img src="../../assets/image/1.jpg" alt="" />
+        </div>
     </div>
   )
 }
