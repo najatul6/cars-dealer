42dc2353b18444500db4785346198e6c742d928e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-25 17:54:25 +0600

    add icons and search icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,43 +3,101 @@ import { navLinks } from "../../utils/menu";
 import { IoClose, IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-      // Edit top position (top-[25%])
-      <header className="fixed top-[20%] left-0 w-full py-[1.3rem] px-[2%] lg:py-5 lg:px-[2%]  flex justify-between items-center z-[100] before:content-[''] before:top-0 before:left-0 before:absolute before:w-full before:h-full before:bg-gradient-back before:backdrop-blur-[50px] before:z-[-1] shadow-xl shadow-gradient-back text-white font-bold">
-        <div className="text-3xl font-bold lg:leading-[70px] capitalize cursor-pointer">
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <header className="relative w-full py-[1.3rem] px-[2%] lg:py-5 lg:px-[2%]  flex justify-between items-center z-[100] before:content-[''] before:top-0 before:left-0 before:absolute before:w-full before:h-full before:bg-gradient-back before:backdrop-blur-[50px] before:z-[-1] shadow-xl shadow-gradient-back text-white font-bold">
+      {/* <div className="text-3xl font-bold lg:leading-[70px] capitalize cursor-pointer lg:hidden">
           <a href="/" className="block">
           company logo
           </a>
-        </div>
-        <label
-          htmlFor=""
-          className="text-4xl text-white cursor-pointer lg:hidden absolute right-[5%] inline-flex duration-300 hover:shadow-[#33ffff_0px_0px_5px] hover:text-cyan-500 rounded-md"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? <IoClose /> : <IoMenu />}
-        </label>
-        <nav className={`${
-            isOpen ? "h-[17 rem]" : " h-0 overflow-hidden "
-          } top-full  absolute left-0 w-full bg-gradient-back backdrop-blur-2xl shadow-2xl shadow-gradient-back transition-all duration-500 lg:h-auto lg:static lg:w-auto lg:bg-inherit lg:backdrop-blur-0 lg:shadow-none delay-700 lg:delay-0`}
-        >
-          <ul className="flex flex-col lg:flex-row justify-between items-center gap-6 px-5 py-5 md:py-2 lg:py-0">
-            {navLinks?.map((item) => (
-              <li key={item} className={`${
-                  isOpen ? "translate-y-0 transition-all duration-300" : "translate-y-[-50px] lg:translate-y-0 transition-all duration-700"
-                }`}>
-                <a
-                  href={item?.path}
-                  className="md:leading-[70px] w-full text-lg py-2 px-4 hover:shadow-[#33ffff_0px_0px_5px] hover:text-cyan-500 rounded-md"
-                >
-                  {item?.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
-    );
+        </div> */}
+      <label
+        htmlFor=""
+        className="text-4xl text-white cursor-pointer lg:hidden absolute right-[5%] inline-flex duration-300 hover:shadow-[#33ffff_0px_0px_5px] hover:text-cyan-500 rounded-md"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? <IoClose /> : <IoMenu />}
+      </label>
+      <nav
+        className={`${
+          isOpen ? "h-[17 rem]" : " h-0 overflow-hidden "
+        } top-full  absolute left-0 w-full bg-gradient-back backdrop-blur-2xl shadow-2xl shadow-gradient-back transition-all duration-500 lg:h-auto lg:static lg:w-auto lg:bg-inherit lg:backdrop-blur-0 lg:shadow-none delay-700 lg:delay-0`}
+      >
+        <ul className="flex flex-col lg:flex-row justify-between items-center gap-6 px-5 py-5 md:py-2 lg:py-0">
+          {navLinks?.map((item) => (
+            <li
+              key={item?.route}
+              className={`${
+                isOpen
+                  ? "translate-y-0 transition-all duration-300"
+                  : "translate-y-[-50px] lg:translate-y-0 transition-all duration-700"
+              }`}
+            >
+              <a
+                href={item?.route}
+                className="md:leading-[70px] w-full text-lg py-2 px-4 hover:shadow-[#33ffff_0px_0px_5px] hover:text-cyan-500 rounded-md"
+              >
+                {item?.label}
+              </a>
+            </li>
+          ))}
+          <div>
+            <label className="input input-bordered flex items-center gap-2">
+              <input type="text" className="grow" placeholder="Search" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
+            <div className="border-2 h-full rounded-full"></div>
+            <div className="grid grid-flow-col gap-4">
+      <a>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="fill-current">
+          <path
+            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+        </svg>
+      </a>
+      <a>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="fill-current">
+          <path
+            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+        </svg>
+      </a>
+      <a>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="fill-current">
+          <path
+            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+        </svg>
+      </a>
+    </div>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
