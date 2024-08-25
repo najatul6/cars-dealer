afc09ed6bfd12c0b748e30eb61dd4c9e832bd5dd
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-25 18:31:16 +0600

    change image height

--- src/utils/slider.js
+++ src/utils/slider.js
@@ -1,22 +1,22 @@
 export const sliders=[
     {
         id:1,
-        title:"Slider 1",
+        title:"sports cars",
         image:"https://d3ogcz7gf2u1oh.cloudfront.net/dealers/rhtoyota/assets/banner_img.jpg"
     },
     {
         id:2,
-        title:"Slider 2",
+        title:"cars for riders",
         image:"https://img.pikbest.com/background/20220119/car-banner-background_6224454.jpg!w700wp"
     },
     {
         id:3,
-        title:"Slider 3",
+        title:"cars sample",
         image:"https://i.pinimg.com/originals/44/fb/2d/44fb2d03ef41162f0124e9bedb18cc5e.jpg"
     },
     {
         id:4,
-        title:"Slider 4",
+        title:"High speed Cars",
         image:"https://static.vecteezy.com/system/resources/thumbnails/006/763/501/small_2x/high-speed-car-banner-template-with-glowing-low-poly-graphic-car-vector.jpg"
     }
 ]
