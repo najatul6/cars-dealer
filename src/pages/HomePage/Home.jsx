f897482e278d7dda233088128a6b5dea8823d2bb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-26 22:29:17 +0600

    change position and something

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -3,10 +3,10 @@ import Slider from "../../Components/Slider/Slider";
 
 const Home = () => {
   return (
-    <div className="">
+    <div className="h-screen">
       <div className="relative">
         <Slider />
-        <div className="bg-Dark-blue mx-auto w-11/12 absolute -bottom-[35%] left-0 right-0 z-10">
+        <div className="bg-Dark-blue mx-auto w-11/12 absolute -bottom-[35%] left-0 right-0 z-10 shadow-2xl">
           <CarSearchSection />
         </div>
       </div>
