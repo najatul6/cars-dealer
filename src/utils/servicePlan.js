635ea0d1a98fed23225449830bf647e98b70f38d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-26 22:35:03 +0600

    create servicePlan utills

---
+++ src/utils/servicePlan.js
@@ -0,0 +1,9 @@
+export const servicePlan=[
+{
+    id:"458j76_4r5",
+    icon:,
+    title:"Easy Financing",
+    description:"Our easy financing options make it easy for you to borrow a car and start saving.",
+    btnText:"See Price Table"
+}
+]
