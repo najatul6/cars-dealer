6131e8005a189248294bd0fc194f8bb8131fd8e9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-26 21:30:47 +0600

    add car icons

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,3 +1,5 @@
+
+
 export const navLinks = [
     {
       label: "Home",
@@ -24,3 +26,31 @@ export const navLinks = [
       route: "/contact",
     }
   ];
+
+  export const carsIcons=[
+    {
+      id:"250kir",
+      icon: "car-1.svg",
+      alt: "car icon 1",
+    },
+    {
+      id:"350Z",
+      icon: "car-2.svg",
+      alt: "car icon 2",
+    },
+    {
+      id:"SUV",
+      icon: "car-3.svg",
+      alt: "car icon 3",
+    },
+    {
+      id:"E-tron",
+      icon: "car-4.svg",
+      alt: "car icon 4",
+    },
+    {
+      id:"Model S",
+      icon: "car-5.svg",
+      alt: "car icon 5",
+    }
+  ]
--- src/utils/slider.js
+++ src/utils/slider.js
@@ -1,41 +1,45 @@
-import img1 from "../assets/banner/34013015_black_friday_facebook_banner_10.jpg"
-import img2 from "../assets/banner/9603766.jpg"
-import img3 from "../assets/banner/35816078_8316176.jpg"
+import img1 from "../assets/banner/34013015_black_friday_facebook_banner_10.jpg";
+import img2 from "../assets/banner/9603766.jpg";
+import img3 from "../assets/banner/35816078_8316176.jpg";
 
-export const sliders=[
-    {
-        id:1,
-        title:"sports cars",
-        image:"https://d3ogcz7gf2u1oh.cloudfront.net/dealers/rhtoyota/assets/banner_img.jpg"
-    },
-    {
-        id:2,
-        title:"cars for riders",
-        image:"https://img.pikbest.com/background/20220119/car-banner-background_6224454.jpg!w700wp"
-    },
-    {
-        id:3,
-        title:"cars sample",
-        image:"https://i.pinimg.com/originals/44/fb/2d/44fb2d03ef41162f0124e9bedb18cc5e.jpg"
-    },
-    {
-        id:5,
-        title:"High speed Cars",
-        image:"https://static.vecteezy.com/system/resources/thumbnails/006/763/501/small_2x/high-speed-car-banner-template-with-glowing-low-poly-graphic-car-vector.jpg"
-    },
-    {
-        id:6,
-        title:"Popular car template",
-        image:`${img1}`
-    },
-    {
-        id:7,
-        title:"black friday template",
-        image:`${img2}`
-    },
-    {
-        id:8,
-        title:"Motion card template",
-        image:`${img3}`
-    },
-]
+export const sliders = [
+  {
+    id: 1,
+    title: "sports cars",
+    image:
+      "https://d3ogcz7gf2u1oh.cloudfront.net/dealers/rhtoyota/assets/banner_img.jpg",
+  },
+  {
+    id: 2,
+    title: "cars for riders",
+    image:
+      "https://img.pikbest.com/background/20220119/car-banner-background_6224454.jpg!w700wp",
+  },
+  {
+    id: 3,
+    title: "cars sample",
+    image:
+      "https://i.pinimg.com/originals/44/fb/2d/44fb2d03ef41162f0124e9bedb18cc5e.jpg",
+  },
+  {
+    id: 5,
+    title: "High speed Cars",
+    image:
+      "https://static.vecteezy.com/system/resources/thumbnails/006/763/501/small_2x/high-speed-car-banner-template-with-glowing-low-poly-graphic-car-vector.jpg",
+  },
+  {
+    id: 6,
+    title: "Popular car template",
+    image: `${img1}`,
+  },
+  {
+    id: 7,
+    title: "black friday template",
+    image: `${img2}`,
+  },
+  {
+    id: 8,
+    title: "Motion card template",
+    image: `${img3}`,
+  },
+];
