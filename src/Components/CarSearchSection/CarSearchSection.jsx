4bac2f12e897d9692d02fa6c99895542b6bec75b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-26 21:43:55 +0600

    change icon and name positions

--- src/Components/CarSearchSection/CarSearchSection.jsx
+++ src/Components/CarSearchSection/CarSearchSection.jsx
@@ -3,12 +3,13 @@ import { carsIcons } from "../../utils/menu"
 const CarSearchSection = () => {
   return (
     <div>
-        <h1>Lets Find your car.</h1>
+        <h1 className="">Lets Find your car.</h1>
         <div>
             {carsIcons?.map(item=>{
               return(
-                <div key={item?.id}>
-                  <img src={item?.icon} alt={item?.alt} />
+                <div key={item?.id} className="flex flex-col justify-center items-center gap-3">
+                  <img src={item?.icon} alt={item?.alt} className="w-16"/>
+                  <p className="text-xl uppercase font-bold ">{item?.alt}</p>
                 </div>
               )
             })}
