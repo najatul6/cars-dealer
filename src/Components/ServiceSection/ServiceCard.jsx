7dac57f27161c11d65c477c962ede8b3ae5efdad
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-27 22:08:16 +0600

    change icon size for responsiveness

--- src/Components/ServiceSection/ServiceCard.jsx
+++ src/Components/ServiceSection/ServiceCard.jsx
@@ -2,7 +2,7 @@ const ServiceCard = ({ item }) => {
   const { icon: Icon, title, description, btnText } = item;
   return (
     <div className="border">
-      <Icon />
+      <Icon className="text-6xl lg:text-8xl text-white"/>
       <h3>{title}</h3>
       <p>{description}</p>
       <button>{btnText}</button>
