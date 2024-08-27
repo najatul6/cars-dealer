2063cbf51afc0559f5225a8c635f51541166a478
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-27 22:22:02 +0600

    complete Second slider  component

--- src/Components/SecondSlider/SecondSlider.jsx
+++ src/Components/SecondSlider/SecondSlider.jsx
@@ -1,7 +1,39 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
 
 const SecondSlider = () => {
   return (
-    <div>SecondSlider</div>
+    <div className="h-[731px]">
+      {" "}
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
+        {sliders?.map((slider) => (
+          <SwiperSlide key={slider?.id}>
+            <img
+              src={slider?.image}
+              alt={slider?.title}
+              className="w-full h-[731px] object-cover object-center"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   )
 }
 
