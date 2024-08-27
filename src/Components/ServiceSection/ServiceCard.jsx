8b70081ecba32db9edb9cd29c8beb9e7574157ae
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-27 22:18:25 +0600

    prop type validation done in service card

--- src/Components/ServiceSection/ServiceCard.jsx
+++ src/Components/ServiceSection/ServiceCard.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const ServiceCard = ({ item }) => {
   const { icon: Icon, title, description, btnText } = item;
   return (
-    <div className="border flex flex-col justify-center items-center gap-3 p-5 border">
+    <div className="border flex flex-col justify-center items-center gap-3 p-5">
       <Icon className="text-6xl lg:text-8xl text-white"/>
       <h3 className="text-2xl capitalize font-bold text-white">{title}</h3>
       <p className="text-xl text-center">{description}</p>
@@ -11,5 +12,9 @@ const ServiceCard = ({ item }) => {
     </div>
   );
 };
+
+ServiceCard.propTypes={
+  item: PropTypes.any,
+}
 
 export default ServiceCard;
