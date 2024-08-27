458fdbd4babcb2b7b9def73853cdcd9af20bfd31
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-27 22:53:15 +0600

    change all items in slider content

--- src/Components/SecondSlider/SliderContent.jsx
+++ src/Components/SecondSlider/SliderContent.jsx
@@ -2,11 +2,11 @@ import PropTypes from "prop-types";
 const SliderContent = ({ item }) => {
   const { title, subtitle, description, image,price } = item;
   return (
-    <div className="flex bg-white justify-center items-center">
+    <div className="flex bg-white justify-center items-center px-5">
       <div className="bg-white shadow-2xl flex flex-col justify-start items-start p-20 gap-5">
         <h1 className="text-4xl uppercase font-bold text-light-blue">{title}</h1>
-        <h3>{subtitle}</h3>
-        <p>{description}</p>
+        <h3 className="bg-Dark-blue py-4 px-2 text-xl font-light uppercase text-white">{subtitle}</h3>
+        <p className="text-justify">{description}</p>
       </div>
       <div className="w-full flex justify-center items-center relative">
         <img src={image} alt={title} />
