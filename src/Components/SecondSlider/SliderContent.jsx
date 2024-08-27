c2f4bb4f9fed2eae82e6876e182f63661e81bd9a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-27 22:35:04 +0600

    added all content in slider content

--- src/Components/SecondSlider/SliderContent.jsx
+++ src/Components/SecondSlider/SliderContent.jsx
@@ -1,15 +1,17 @@
-
-const SliderContent = ({item}) => {
-    const {title,subtitle,description,image}=item
+const SliderContent = ({ item }) => {
+  const { title, subtitle, description, image } = item;
   return (
     <div>
-        <div className="bg-white shadow-2xl flex-col justify-start items-start">
-            <h1>{title}</h1>
-            <h3>{subtitle}</h3>
-            <p>{description}</p>
-        </div>
+      <div className="bg-white shadow-2xl flex-col justify-start items-start">
+        <h1>{title}</h1>
+        <h3>{subtitle}</h3>
+        <p>{description}</p>
+      </div>
+      <div>
+        <img src={image} alt={title} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SliderContent
+export default SliderContent;
