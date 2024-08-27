5b556cdedc2ee65884cd439333a33268acddd723
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-27 22:15:39 +0600

    remove border round

--- src/Components/ServiceSection/ServiceCard.jsx
+++ src/Components/ServiceSection/ServiceCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const ServiceCard = ({ item }) => {
   const { icon: Icon, title, description, btnText } = item;
   return (
-    <div className="border flex flex-col justify-center items-center gap-3 p-5">
+    <div className="border flex flex-col justify-center items-center gap-3 p-5 border">
       <Icon className="text-6xl lg:text-8xl text-white"/>
       <h3 className="text-2xl capitalize font-bold text-white">{title}</h3>
       <p className="text-xl text-center">{description}</p>
