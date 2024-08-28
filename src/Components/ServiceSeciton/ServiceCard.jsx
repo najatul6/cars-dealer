9459f95df45f2f52b15356716bf0cdd11539e320
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-28 22:12:12 +0600

    send data successfully

--- src/Components/ServiceSeciton/ServiceCard.jsx
+++ src/Components/ServiceSeciton/ServiceCard.jsx
@@ -1,11 +1,22 @@
-
-const ServiceCard = ({service}) => {
-    const {title,description,image}=service
+const ServiceCard = ({ service }) => {
+  const { title, description, image } = service;
   return (
-    <div>
-        
+    <div className="card shadow-xl">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{titel}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServiceCard
+export default ServiceCard;
--- src/Components/ServiceSeciton/ServiceSection.jsx
+++ src/Components/ServiceSeciton/ServiceSection.jsx
@@ -4,7 +4,7 @@ import ServiceCard from "./ServiceCard"
 const ServiceSection = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 place-items-center">
-       {servicesData?.map(service=><ServiceCard key={service?.id}/>)}
+       {servicesData?.map(service=><ServiceCard key={service?.id} service={service}/>)}
     </div>
   )
 }
