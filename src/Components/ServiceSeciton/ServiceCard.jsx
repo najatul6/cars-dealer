09d3070c9303058808555f0c7f2ce5b286684f17
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-28 22:21:17 +0600

    Prop type validation done

--- src/Components/ServiceSeciton/ServiceCard.jsx
+++ src/Components/ServiceSeciton/ServiceCard.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom";
-
+import PropTypes from "prop-types";
 const ServiceCard = ({ service }) => {
   const { title, description, image } = service;
   return (
     <div className="card shadow-2xl rounded-none">
       <figure>
-        <img
-          src={image}
-          alt={title}
-          className="h-[317px]"
-        />
+        <img src={image} alt={title} className="h-[317px]" />
       </figure>
       <div className="card-body">
         <h2 className="card-title text-white">{title}</h2>
         <p>{description}</p>
         <div className="card-actions">
-          <Link className="text-Dark-blue hover:text-light-blue font-bold">Learn More &rarr;</Link>
+          <Link className="text-Dark-blue hover:text-light-blue font-bold">
+            Learn More &rarr;
+          </Link>
         </div>
       </div>
     </div>
   );
+};
+
+ServiceCard.propTypes = {
+  service: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }),
 };
 
 export default ServiceCard;
