9d5d04fc9d1befb27b8b26f59f25549bc9a711ce
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-28 22:24:16 +0600

    create product data utils

---
+++ src/utils/productData.js
@@ -0,0 +1,34 @@
+export const productData = [
+    {
+      id: "a1b2c3d4",
+      name: "Super Clean Car Wash Kit",
+      price: "$29.99",
+      rating: 4.5
+    },
+    {
+      id: "e5f6a7b8",
+      name: "Premium Car Wax",
+      price: "$19.99",
+      rating: 4.7
+    },
+    {
+      id: "c9d0e1f2",
+      name: "Ultra Shine Tire Cleaner",
+      price: "$14.99",
+      rating: 4.3
+    },
+    {
+      id: "g3h4i5j6",
+      name: "Leather Seat Conditioner",
+      price: "$24.99",
+      rating: 4.6
+    },
+    {
+      id: "k7l8m9n0",
+      name: "All-Weather Floor Mats",
+      price: "$49.99",
+      rating: 4.8
+    }
+  ];
+  
+  
