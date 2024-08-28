2b682c6d39c7995ec3fc7b259d5eb4846cb98def
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-28 20:57:55 +0600

    Prop Type valid done

--- src/Components/shared/Header/Header.jsx
+++ src/Components/shared/Header/Header.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from "prop-types";
 const Header = ({text}) => {
   return (
     <div className="flex  flex-col justify-center items-center gap-2 py-5">
@@ -6,6 +6,10 @@ const Header = ({text}) => {
         <hr className="h-1 w-14 bg-light-blue rounded-xl"/>
     </div>
   )
+}
+
+Header.propTypes = {
+  text: PropTypes.string,
 }
 
 export default Header
