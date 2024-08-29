032661253d6ca403f88b1d1bd82ccabe375acf95
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-29 21:28:51 +0600

    added icon in team data

--- src/utils/ourTeam.js
+++ src/utils/ourTeam.js
@@ -1,6 +1,10 @@
 import team1 from "../assets/team/1.jpg"
 import team2 from "../assets/team/2.jpg"
 import team3 from "../assets/team/3.jpg"
+import { SiFacebook } from "react-icons/si";
+import { BiLogoInstagramAlt } from "react-icons/bi";
+import { TfiTwitterAlt } from "react-icons/tfi";
+
 
 export const ourTeam = [
     {
@@ -11,20 +15,67 @@ export const ourTeam = [
         description:"The co-founder of the organization that owns the organization that owns the organization that owns the organization.",
         socialMedia:[
             {
-                Icon:"",
+                Icon:SiFacebook,
                 link:"http://facebook.com/najatul6/",
             },
             {
-                Icon:"",
+                Icon:BiLogoInstagramAlt,
                 link:"http://instagram.com/najatul6/",
             },
             
             {
-                Icon:"",
+                Icon:TfiTwitterAlt,
                 link:"http://twitter.com/najatul6/",
             },
             
         ]
 
     },
+    {
+        id:"ijr7_iureka",
+        name:"John Doe",
+        image:`${team2}`,
+        position:"CFO",
+        description:"The Chief Financial Officer of the organization that owns the organization that owns the organization.",
+        socialMedia:[
+            {
+                Icon:SiFacebook,
+                link:"http://facebook.com/johndoe6/",
+            },
+            {
+                Icon:SiFacebook,
+                link:"http://instagram.com/johndoe6/",
+            },
+            
+            {
+                Icon:SiFacebook,
+                link:"http://twitter.com/johndoe6/",
+            },
+            
+        ]
+    },
+    {
+        id:"ijr7_iureka",
+        name:"Jane Smith",
+        image:`${team3}`,
+        position:"CTO",
+        description:"The Chief Technology Officer of the organization that owns the organization that owns the organization.",
+        socialMedia:[
+            {
+                Icon:SiFacebook,
+                link:"http://facebook.com/janesmith6/",
+            },
+            {
+                Icon:SiFacebook,
+                link:"http://instagram.com/janesmith6/",
+            },
+            
+            {
+                Icon:SiFacebook,
+                link:"http://twitter.com/janesmith6/",
+            },
+            
+        ]
+    }
+
 ]
