daa96851cfc14e7a491427fc023d5e0f5c8f54bd
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-30 22:59:10 +0600

    add image in from

--- src/Components/ShopSection/ItemCard.jsx
+++ src/Components/ShopSection/ItemCard.jsx
@@ -3,7 +3,9 @@ const ItemCard = ({item}) => {
   const {image,name,price, rating,stoke,weight,colorOptions} = item
   return (
     <div>
-
+      <div>
+        <img src={image[0]} alt={name} />
+      </div>
     </div>
   )
 }
