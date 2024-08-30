f3bcdf20bfb1b2fdd8c077daa2342a6eac0f69bd
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-30 19:52:02 +0600

    create  categories utility for catalog

---
+++ src/utils/category.js
@@ -0,0 +1,18 @@
+export const category=[
+    {
+        id:"87j8743_mur",
+        label:"sedan",
+    },
+    {
+        id:"87j8743_8j",
+        label:"hatchback"
+    },
+    {
+        id:"j8ne8_f8r5",
+        label:"coupe"
+    },
+    {
+        id:"cdjjro_r85n9",
+        label:"Station wagon"
+    }
+]
