b5238b2434b429d98734f55cf9feb0961baa1472
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-30 19:59:20 +0600

    create first product

--- src/utils/products.js
+++ src/utils/products.js
@@ -1,3 +1,18 @@
-export const products =[
-
-]
+export const products = [
+  {
+    id: "43_muern_kris",
+    title: "infinity engine x coupe ",
+    image: "",
+    price: "200",
+    month: "",
+    modified: "2015",
+    setting:"manual",
+    sold:"5",
+    liquid:"5.6 lt",
+    fuel:"95%",
+    engine:"v12",
+    transmission:"automatic",
+    description: "The Infiniti QX80 is a luxury sports car by American manufacturer Infiniti. It has a front-wheel-drive layout with a 3.0L V8 engine, a 2+2 seating arrangement, and a 4.0-liter engine.",
+    rating: 4.5,
+  },
+];
