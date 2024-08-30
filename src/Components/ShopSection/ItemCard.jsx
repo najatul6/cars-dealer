75480267dee68ab251f9af5d6d9e50a05070a110
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-30 23:00:19 +0600

    create card for shop section

--- src/Components/ShopSection/ItemCard.jsx
+++ src/Components/ShopSection/ItemCard.jsx
@@ -2,9 +2,21 @@
 const ItemCard = ({item}) => {
   const {image,name,price, rating,stoke,weight,colorOptions} = item
   return (
-    <div>
-      <div>
-        <img src={image[0]} alt={name} />
+    <div className="card shadow-2xl rounded-none w-full">
+      <figure>
+        <img src={image} alt={name} className="h-[317px] w-full" />
+      </figure>
+      <div className="card-body items-center text-center text-white">
+
+          <Rating style={{ maxWidth: 120 }} value={rating} readOnly />
+
+        <h2 className="card-title ">{name}</h2>
+        <p>{price}</p>
+        <div className="card-actions">
+          <button className="btn bg-regal-blue hover:bg-light-blue text-white outline-none border-none rounded-none font-bold">
+            Buy Now
+          </button>
+        </div>
       </div>
     </div>
   )
