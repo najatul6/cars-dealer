3f4459b7ad22b6e14083b4c50adb556d1a5b349b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-02 05:27:10 +0600

    add some class in shop tabs

--- src/Components/ShopSection/ItemCard.jsx
+++ src/Components/ShopSection/ItemCard.jsx
@@ -1,22 +1,26 @@
-import { Rating } from "@smastrom/react-rating"
+import { Rating } from "@smastrom/react-rating";
 import PropTypes from "prop-types";
 
-const ItemCard = ({item}) => {
-  const {images,name,price, rating,stoke,weight,colorOptions,isOnSale} = item
-  // console.log(item.image);
+const ItemCard = ({ item }) => {
+  const { images, name, price, rating, stock, weight, colorOptions, isOnSale } =
+    item;
+  // console.log(stoke);
   return (
-    <div className="card shadow-2xl rounded-none w-full">
+    <div className="card shadow-2xl rounded-xl w-full">
       <figure>
         <img src={images[0]} alt={name} className="h-[317px] w-full" />
       </figure>
-      <div className="card-body items-center text-center text-white">
-
-          <Rating style={{ maxWidth: 120 }} value={rating} readOnly />
-
-        <h2 className="card-title ">{name}</h2>
-        <p>{price}</p>
-        <div className="flex justify-center items-center gap-1">
-        {colorOptions.map((color, index) => (
+      <div className="card-body text-center text-white">
+        <Rating style={{ maxWidth: 120 }} value={rating} readOnly />
+        <div className="flex flex-col justify-start items-start">
+          <h2 className="card-title"> {name}</h2>
+          <p>Price : {price}</p>
+          <p>Weight : {weight}</p>
+          <p>Stock : {stock}</p>
+        </div>
+        <div className="flex justify-start items-center gap-2">
+          Color :{" "}
+          {colorOptions.map((color, index) => (
             <div
               key={index}
               className="w-4 h-4 rounded-full"
@@ -24,22 +28,24 @@ const ItemCard = ({item}) => {
               title={color}
             ></div>
           ))}
-          {/* <p className={`w-2 h-2 rounded-full text-${colorOptions[0]}-600`}>{colorOptions[0]}</p>
-          <p className={`w-2 h-2 rounded-full text-${colorOptions[1]}-600`}>{colorOptions[1]}</p>
-          <p className={`w-2 h-2 rounded-full text-${colorOptions[2]}-600`}>{colorOptions[2]}</p> */}
         </div>
-        <div className="card-actions">
-          <button className={`${isOnSale || "disabled"} btn bg-regal-blue hover:bg-light-blue text-white outline-none border-none rounded-none font-bold`}>
-            {isOnSale?"Add to Cart":"Out of stock"}
+        <div className="text-white w-full">
+          <button
+            disabled={!isOnSale}
+            className={`${
+               isOnSale ? "bg-regal-blue hover:bg-light-blue" : "bg-red-600 text-red-600"
+            } btn w-full rounded-xl  text-white outline-none border-none font-bold`}
+          >
+            {isOnSale ? "Add to Cart" : "Out of stock"}
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-ItemCard.propTypes={
-  item:PropTypes.any.isRequired,
-}
+ItemCard.propTypes = {
+  item: PropTypes.any.isRequired,
+};
 
-export default ItemCard
+export default ItemCard;
--- src/Components/ShopSection/ShopSection.jsx
+++ src/Components/ShopSection/ShopSection.jsx
@@ -1,24 +1,48 @@
-import { Tab, Tabs, TabList, TabPanel,  } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+// import "react-tabs/style/react-tabs.css";
 import { categories } from "../../utils/categories";
 import { shopProducts } from "../../utils/shopProducts";
 import ShopTab from "./ShopTab";
+import { useState } from "react";
 
 const ShopSection = () => {
-  const sedansProducts= shopProducts.filter(product=>product?.category === "sedan")
-  const hatchbackProducts=shopProducts.filter(product=>product?.category === "hatchback")
-  const coupeProducts=shopProducts.filter(product=>product?.category === "coupe")
-  const stationWagonProducts=shopProducts.filter(product=>product?.category === "stationwagon")
-  const hotDealProducts=shopProducts.filter(product=>product?.isHotDeal === true)
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const handleTabClick = (index) => {
+    setActiveTabIndex(index);
+  };
+  const sedansProducts = shopProducts.filter(
+    (product) => product?.category === "sedan"
+  );
+  const hatchbackProducts = shopProducts.filter(
+    (product) => product?.category === "hatchback"
+  );
+  const coupeProducts = shopProducts.filter(
+    (product) => product?.category === "coupe"
+  );
+  const stationWagonProducts = shopProducts.filter(
+    (product) => product?.category === "stationwagon"
+  );
+  const hotDealProducts = shopProducts.filter(
+    (product) => product?.isHotDeal === true
+  );
   return (
     <div className="text-center">
-      <Tabs >
-        <TabList>
-          {
-            categories?.map(item=><Tab key={item?.id}>{item?.category}</Tab>)
-          }
-          
+      <Tabs selectedIndex={activeTabIndex} onSelect={(index) => handleTabClick(index)}>
+        <TabList className="flex space-x-4 justify-center">
+          {categories?.map((item,index) => (
+            <Tab
+              key={item?.id}
+              className={`px-4 py-2 cursor-pointer transition-colors duration-300 border-b-0 ${
+                activeTabIndex === index
+                  ? "border-2 border-b-0 rounded-t-xl text-cyan-500 "
+                  : "border-b-0 text-white hover:text-cyan-500 "
+              }`}
+            >
+              {item?.category}
+            </Tab>
+          ))}
         </TabList>
+          <hr className="w-full mb-10 border-2 border-white"/>
         <TabPanel>
           <ShopTab items={hotDealProducts} />
         </TabPanel>
@@ -34,7 +58,6 @@ const ShopSection = () => {
         <TabPanel>
           <ShopTab items={stationWagonProducts} />
         </TabPanel>
-        
       </Tabs>
     </div>
   );
