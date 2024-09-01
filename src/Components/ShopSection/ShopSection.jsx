c67360525c3e25b5a3ca5fd9a36f7af10475d356
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-01 23:05:00 +0600

    install react-tabs for show tabs

--- src/Components/ShopSection/ShopSection.jsx
+++ src/Components/ShopSection/ShopSection.jsx
@@ -1,14 +1,41 @@
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { categories } from "../../utils/categories";
 
 const ShopSection = () => {
   return (
     <div>
-    #Todo: import react tabs
-
-
+      <Tabs defaultIndex={item?.id} onSelect={item.id}>
+        <TabList>
+          {
+            categories?.map(item=><Tab key={item?.id}>{item?.category}</Tab>)
+          }
+          
+        </TabList>
+        {/* <TabPanel>
+          <OrderTab items={popularItem} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={offeredItem} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={saladItem} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={pizzaItem} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={dessertItem} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={soupItem} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={drinksItem} />
+        </TabPanel> */}
+      </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default ShopSection
+export default ShopSection;
