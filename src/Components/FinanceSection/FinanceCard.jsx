dbb82dd36db945fedf075a4c89ec99098de420fe
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-03 23:28:32 +0600

    done to add text in card

--- src/Components/FinanceSection/FinanceCard.jsx
+++ src/Components/FinanceSection/FinanceCard.jsx
@@ -1,15 +1,23 @@
-
-const FinanceCard = ({item}) => {
+const FinanceCard = ({ item }) => {
   return (
     <div>
-            <p>{item?.name}</p>
-            <h1>${item?.price}</h1>
-            <p>/{item?.billingCycle}</p>
-            <div>
-                {item?.features?.map((task,idx)=><li key={idx}>{task}</li>)}
-            </div>
+      <p>{item?.name}</p>
+      <h1>${item?.price}</h1>
+      <p>/{item?.billingCycle}</p>
+      <div>
+        <p>
+          <span>Features:</span>
+          {item?.features?.map((task, idx) => (
+            <li key={idx}>{task}</li>
+          ))}
+        </p>
+        <p>{item?.additionalInfo}</p>
+      </div>
+      <button>
+        Apply Now &
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default FinanceCard
+export default FinanceCard;
