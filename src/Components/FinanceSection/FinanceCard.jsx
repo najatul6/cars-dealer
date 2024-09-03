aff01f94456053ffe40983ec71d1b14ee5aa0df3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-03 23:41:40 +0600

    complete price cards design

--- src/Components/FinanceSection/FinanceCard.jsx
+++ src/Components/FinanceSection/FinanceCard.jsx
@@ -1,11 +1,13 @@
 const FinanceCard = ({ item }) => {
   return (
-    <div className="border rounded-xl text-white ">
-        <p className="text-3xl text-center font-bold pb-5">{item?.name}</p>
-        <hr className="w-full" />
+    <div className="border py-5 text-white flex flex-col flex-shrink-0 ">
+      <p className="text-3xl text-center font-bold pb-5">{item?.name}</p>
+      <hr className="w-full" />
       <div className="text-center py-5 px-4 flex flex-col justify-center items-center gap-6">
-        <h1 className="text-5xl">${item?.price}</h1>
-        <p>/{item?.billingCycle}</p>
+        <div>
+          <h1 className="text-5xl">${item?.price}</h1>
+          <p>/{item?.billingCycle}</p>
+        </div>
         <div className="flex gap-4 flex-col justify-center items-center">
           <p className="flex flex-col justify-center items-start">
             <span>Features:</span>
@@ -16,7 +18,9 @@ const FinanceCard = ({ item }) => {
           <p>{item?.additionalInfo} </p>
         </div>
       </div>
-        <button className="py-3 btn w-full">Apply Now &#10149;</button>
+      <div className="flex justify-center items-center">
+        <button className="py-3 btn">Apply Now &#10149;</button>
+      </div>
     </div>
   );
 };
--- src/Components/FinanceSection/FinanceSection.jsx
+++ src/Components/FinanceSection/FinanceSection.jsx
@@ -3,7 +3,7 @@ import FinanceCard from "./FinanceCard"
 
 const FinanceSection = () => {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {
             priceplan?.map((items,idx)=><FinanceCard key={idx} item={items}/>)
         }
