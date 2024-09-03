1f777af6e25c884a7c8df4cfc2b9756b0ecf4895
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-03 20:04:20 +0600

    complete card wizard first section

--- src/Components/CarWizardSection/CardWizardSection.jsx
+++ src/Components/CarWizardSection/CardWizardSection.jsx
@@ -3,10 +3,10 @@ const CardWizardSection = () => {
   return (
     <div>
       <section>
-        <h1></h1>
-        <p></p>
-        <p></p>
-        <button></button>
+        <h1>Are you looking for a Car?</h1>
+        <p>car wizard is here to help you</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, aliquam ex molestias itaque perferendis suscipit laboriosam? Iure suscipit, ratione in eaque deleniti rem dolore nemo tempora, amet ipsa impedit rerum.</p>
+        <button>Find your dream car</button>
       </section>
       <section>
         <FaCarSide />
