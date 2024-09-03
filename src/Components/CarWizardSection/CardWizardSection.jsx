204cd7bdc23920be7595928726b475ff709a47b8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-03 20:05:49 +0600

    complete section 2

--- src/Components/CarWizardSection/CardWizardSection.jsx
+++ src/Components/CarWizardSection/CardWizardSection.jsx
@@ -4,15 +4,15 @@ const CardWizardSection = () => {
     <div>
       <section>
         <h1>Are you looking for a Car?</h1>
-        <p>car wizard is here to help you</p>
+        <p>car wizard is here to help you.</p>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, aliquam ex molestias itaque perferendis suscipit laboriosam? Iure suscipit, ratione in eaque deleniti rem dolore nemo tempora, amet ipsa impedit rerum.</p>
         <button>Find your dream car</button>
       </section>
       <section>
         <FaCarSide />
-        <h1></h1>
-        <p></p>
-        <p></p>
+        <h1>sell your car easily</h1>
+        <p>request a quote for your car.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis omnis cumque exercitationem eveniet? Adipisci enim, maiores illum provident deserunt autem dolorem eveniet? Illum minima harum, totam non voluptatibus modi quidem!</p>
         <button>sell you car</button>
       </section>
     </div>
