92c73c78899c46fc3aa241ddd1ce7f17ecf78fbb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-03 21:30:31 +0600

    change shape card wizard section section no 2

--- src/Components/CarWizardSection/CardWizardSection.jsx
+++ src/Components/CarWizardSection/CardWizardSection.jsx
@@ -1,10 +1,10 @@
-import { FaCarSide } from "react-icons/fa6";
 import img from "../../assets/image/3.jpg";
+import img2 from "../../assets/cars/car1.png";
 const CardWizardSection = () => {
   return (
-    <div className="flex justify-center items-center bg-regal-blue">
+    <div className="flex justify-center items-center ">
       <section
-        className="py-20 px-10 bg-regal-blue text-white flex flex-col gap-4 justify-center items-start w-full bg-blend-multiply "
+        className="py-20 px-10 bg-regal-blue border text-white flex flex-col gap-4 justify-center items-start w-full bg-blend-multiply "
         style={{
           backgroundImage: `url(${img})`,
           backgroundPosition: "center",
@@ -20,19 +20,26 @@ const CardWizardSection = () => {
           suscipit, ratione in eaque deleniti rem dolore nemo tempora, amet ipsa
           impedit rerum.
         </p>
-        <button className="btn text-white">Find your dream car &#10149;</button>
+        <button className="btn text-white uppercase">
+          Find your dream car &#10149;
+        </button>
       </section>
-      <section className="py-10 px-5 flex flex-col justify-start items-start">
-        <FaCarSide />
-        <h1>sell your car easily</h1>
-        <p>request a quote for your car.</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
-          omnis cumque exercitationem eveniet? Adipisci enim, maiores illum
-          provident deserunt autem dolorem eveniet? Illum minima harum, totam
-          non voluptatibus modi quidem!
-        </p>
-        <button>sell you car</button>
+      <section className="py-20 px-10 flex flex-col justify-center items-start gap-4 bg-regal-blue">
+
+          <div className="w-[150px]">
+            <img src={img2} alt="Car icon" />
+          </div>
+          <h1 className="text-3xl font-bold">sell your car easily</h1>
+          <p className="uppercase">request a quote for your car.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Perferendis omnis cumque exercitationem eveniet? Adipisci enim,
+            maiores illum provident deserunt autem dolorem eveniet? Illum minima
+            harum, totam non voluptatibus modi quidem!
+          </p>
+          <button className="btn text-white uppercase">
+            sell you car &#10149;
+          </button>
       </section>
     </div>
   );
