e4b5b5b8af5bf90d8d7fb5082441deeefb4bb779
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-03 20:37:18 +0600

    change image background and add some text

--- src/Components/CarWizardSection/CardWizardSection.jsx
+++ src/Components/CarWizardSection/CardWizardSection.jsx
@@ -1,18 +1,37 @@
 import { FaCarSide } from "react-icons/fa6";
+import img from "../../assets/image/3.jpg";
 const CardWizardSection = () => {
   return (
-    <div>
-      <section>
+    <div className="flex justify-center items-center bg-regal-blue">
+      <section
+        className="py-20 bg-regal-blue text-white flex flex-col gap-6 justify-center items-center w-full bg-blend-multiply "
+        style={{
+          backgroundImage: `url(${img})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <h1>Are you looking for a Car?</h1>
         <p>car wizard is here to help you.</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, aliquam ex molestias itaque perferendis suscipit laboriosam? Iure suscipit, ratione in eaque deleniti rem dolore nemo tempora, amet ipsa impedit rerum.</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis,
+          aliquam ex molestias itaque perferendis suscipit laboriosam? Iure
+          suscipit, ratione in eaque deleniti rem dolore nemo tempora, amet ipsa
+          impedit rerum.
+        </p>
         <button>Find your dream car</button>
       </section>
-      <section>
+      <section className="py-10 px-5 flex flex-col justify-start items-start">
         <FaCarSide />
         <h1>sell your car easily</h1>
         <p>request a quote for your car.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis omnis cumque exercitationem eveniet? Adipisci enim, maiores illum provident deserunt autem dolorem eveniet? Illum minima harum, totam non voluptatibus modi quidem!</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
+          omnis cumque exercitationem eveniet? Adipisci enim, maiores illum
+          provident deserunt autem dolorem eveniet? Illum minima harum, totam
+          non voluptatibus modi quidem!
+        </p>
         <button>sell you car</button>
       </section>
     </div>
