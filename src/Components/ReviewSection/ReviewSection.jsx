6088e310227a7d16ba9313f8f682105ddaf3feeb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-04 23:34:26 +0600

    create reviews data

---
+++ src/Components/ReviewSection/ReviewSection.jsx
@@ -0,0 +1,11 @@
+import { reviews } from "../../utils/Reviews"
+
+const ReviewSection = () => {
+  return (
+    <div>
+        {reviews?.map(review=>)}
+    </div>
+  )
+}
+
+export default ReviewSection
--- src/utils/Reviews.js
+++ src/utils/Reviews.js
@@ -1,6 +1,26 @@
-export const reviwes=[
-    {
-
-        
-    }
-]
+export const reviews = [
+  {
+    id: "790_uii",
+    label: "style",
+    style: "Cover - Format: Standard",
+    description:
+      "lot the stories unfold and unkash yourself. Much mantul ouch much one grizly a much and boomed well.",
+    review: 9.5,
+  },
+  {
+    id: "79y0_u2ii",
+    label: "style",
+    style: "Cover - Format: Standard",
+    description:
+      "lot the stories unfold and unkash yourself. Much mantul ouch much one grizly a much and boomed well.",
+    review: 9.5,
+  },
+  {
+    id: "79y0_u2ii",
+    label: "style",
+    style: "Cover - Format: Standard",
+    description:
+      "lot the stories unfold and unkash yourself. Much mantul ouch much one grizly a much and boomed well.",
+    review: 9.5,
+  },
+];
