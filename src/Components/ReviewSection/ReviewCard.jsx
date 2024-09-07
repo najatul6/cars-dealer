c82530a3508c135003409f1cee804bf8ec027e89
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-07 18:31:07 +0600

    update review card

--- src/Components/ReviewSection/ReviewCard.jsx
+++ src/Components/ReviewSection/ReviewCard.jsx
@@ -3,16 +3,16 @@ const ReviewCard = ({review}) => {
     
   return (
     <div>
-        <div>
+        <div className="relative">
             <img src={review?.img} alt={review?.style} />
-            <div className="py-3 px-2 bg-light-blue text-white font-bold -skew-x-12 inline-block">
+            <div className="absolute -bottom-5 left-5 py-2 px-4 bg-light-blue text-white font-bold -skew-x-12 inline-block">
               &raquo; {review?.rating}
             </div>
         </div>
-        <div>
-            <h2>{review?.label} : {review?.style}</h2>
-            <p>{review?.description}</p>
-            <p>Reviews : {review?.rating}</p>
+        <div className="mt-10 space-y-4">
+            <h2 className="text-xl text-white font-bold"> {review?.style}</h2>
+            <p>{review?.description.slice(0,200) + "..."}</p>
+            <a href="#" className="text-light-blue">Read more &raquo;</a>
         </div>
 
     </div>
