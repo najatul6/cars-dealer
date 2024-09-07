a1d4bbd745addc0c97d0f4112466ac55fd36ac88
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-07 19:37:19 +0600

    added footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,28 @@
 const Footer = () => {
   return (
     <div>
+      <footer className="footer bg-neutral text-neutral-content p-10">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
       <footer className="footer footer-center p-4 bg-base-300 text-base-content">
         <aside>
           <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
