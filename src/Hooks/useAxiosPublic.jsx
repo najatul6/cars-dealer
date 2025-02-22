98c3548a83ba2e78f7e5159d6f20c69eead8a11a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-22 22:03:33 +0600

    add useAuth hook for accessing authentication context and create useAxiosPublic hook for public API requests

---
+++ src/Hooks/useAxiosPublic.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const axiosPublic = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
+const useAxiosPublic = () => {
+  return axiosPublic;
+};
+
+export default useAxiosPublic;
