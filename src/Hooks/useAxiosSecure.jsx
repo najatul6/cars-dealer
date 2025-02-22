8ead21d3f70af93cf801845a8287909c5743a340
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-22 22:02:32 +0600

    implement useAxiosSecure hook with request and response interceptors for secure API calls

---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
+const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { logOut } = useAuth();
+
+  // Add a request interceptor
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise?.reject(error);
+    }
+  );
+
+  // Add a response interceptor
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    async (error) => {
+      const status = error?.response?.status;
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/auth/login", { state: { from: location } });
+      }
+      return Promise.reject(error);
+    }
+  );
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
