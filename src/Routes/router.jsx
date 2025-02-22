17cd5e604e5533aaaf5f5307cbac522fe3dd2265
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-22 23:09:01 +0600

    feat: add AboutPage component and update router for new route

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -14,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path:"/about",
+        element:
       }
      
     ],
---
+++ src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,8 @@
+
+const AboutPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen text-white text-7xl font-bold">About Page is coming soon....</div>
+  )
+}
+
+export default AboutPage
