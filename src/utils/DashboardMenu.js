7ce5313265fd47a3585b5272cc9e4518f86652a4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-25 22:26:30 +0600

    feat: add user and admin navigation menus for dashboard

---
+++ src/utils/DashboardMenu.js
@@ -0,0 +1,52 @@
+
+export const userNav = [
+    {
+      label: "Overview",
+      path: "/dashboard/overview",
+      icon: <SquareChartGantt />,
+    },
+    {
+      label: "My Orders",
+      path: "/dashboard/my-orders",
+      icon: <BookA />,
+    },
+  ];
+
+
+export const adminNav = [
+    {
+      label: "Overview",
+      path: "/dashboard/overview",
+      icon: <SquareChartGantt />,
+    },
+    {
+      label: "Pending Orders",
+      path: "/dashboard/new-orders",
+      icon: <PackageOpen />,
+    },
+    {
+      label: "Processing Orders",
+      path: "/dashboard/process-orders",
+      icon: <Truck />,
+    },
+    {
+      label: "Completed Orders",
+      path: "/dashboard/complete-orders",
+      icon: <PackageCheck />,
+    },
+    {
+      label: "Categories Management",
+      path: "/dashboard/categories-control",
+      icon: <Newspaper />,
+    },
+    {
+      label: "Products Management",
+      path: "/dashboard/products-control",
+      icon: <PackageSearch />,
+    },
+    {
+      label: "Users Management",
+      path: "/dashboard/users-control",
+      icon: <UserCog />,
+    },
+  ];
