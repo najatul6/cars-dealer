93945a39c1d594f83e3d631fcd661ccde1186673
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-25 22:21:05 +0600

    feat: enhance Dashboard component layout with sidebar state management

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom"
 
 const Dashboard = () => {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    <div>Dashboard</div>
+    <div  className="relative pt-[70px] h-screen scroll-smooth">
+        <div>
+        <div className="flex items-start">
+          {/* <DashboardSidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/> */}
+
+          <section className="main-content w-full overflow-auto p-6 ">
+              <Outlet/>
+            {/* <div className="overflow-x-auto no-scrollbar">
+            </div> */}
+          </section>
+        </div>
+      </div>
+    </div>
   )
 }
 
