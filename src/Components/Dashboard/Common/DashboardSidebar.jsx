91bbc1e00dba06bfd7512ba221dbfb7a41bc5c6a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-26 21:53:21 +0600

    feat: replace UserShield icon with custom SVG in DashboardSidebar

--- src/Components/Dashboard/Common/DashboardSidebar.jsx
+++ src/Components/Dashboard/Common/DashboardSidebar.jsx
@@ -9,7 +9,7 @@ import {
   SquareChartGantt, BookA, LifeBuoy, Heart, User,
   ClipboardList, Truck, PackageCheck, MessagesSquare, Wallet,
   PackageOpen, RotateCcw, Newspaper, PackageSearch, UserCog,
-  UserShield, Headset, BarChart, CreditCard, Settings
+   Headset, BarChart, CreditCard, Settings
 } from "lucide-react";
 
 const DashboardSidebar = ({ setSidebarOpen, sidebarOpen }) => {
@@ -130,7 +130,7 @@ const DashboardSidebar = ({ setSidebarOpen, sidebarOpen }) => {
     {
         label: "Agents",
         path: "/dashboard/agents-control",
-        icon: <UserShield />,
+        icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-shield-user"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/><path d="M6.376 18.91a6 6 0 0 1 11.249.003"/><circle cx="12" cy="11" r="4"/></svg>,
     },
     {
         label: "Customer Support",
