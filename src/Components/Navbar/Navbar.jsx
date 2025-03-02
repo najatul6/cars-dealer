72bd4b228509a3f48b240875626f6eda5e201cd0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-02 22:19:12 +0600

    refactor Navbar component to use Link for external navigation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { navLinks } from "../../utils/menu";
 import { IoClose, IoMenu } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -59,7 +60,7 @@ const Navbar = () => {
             </label>
             <div className="border-2 h-8 rounded-full hidden lg:flex"></div>
             <div className="grid grid-flow-col gap-4">
-              <a className="hover:text-Dark-blue cursor-pointer" href="#">
+              <Link to="https://x.com/MNajatul" className="hover:text-Dark-blue cursor-pointer" target="_blank">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -69,7 +70,7 @@ const Navbar = () => {
                 >
                   <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
                 </svg>
-              </a>
+              </Link>
               <a className="hover:text-Dark-blue cursor-pointer" href="#">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
