610b6c574d74ad47c0058eeef91b48da3f8d881c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-02 22:08:11 +0600

    create new path for newsroom

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -61,6 +61,10 @@ const router = createBrowserRouter([
         element:<Services/>
       },
       {
+        path:"newsroom",
+        element:<Services/>
+      },
+      {
         path: "un-auth",
         element: <UnAuth />,
       },
