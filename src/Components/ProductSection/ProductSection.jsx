654486cd71b84c56c97bd2ba8079fa5ae1c7283a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-04 21:44:30 +0600

    enhance ProductSection with animation effects for product cards

--- src/Components/ProductSection/ProductSection.jsx
+++ src/Components/ProductSection/ProductSection.jsx
@@ -1,14 +1,26 @@
-import useProduct from "../../Hooks/useProduct"
-import ProductsCard from "./ProductsCard"
+import { motion } from "framer-motion";
+import useProduct from "../../Hooks/useProduct";
+import ProductsCard from "./ProductsCard";
 
 const ProductSection = () => {
- const [products]=useProduct()
-  
+  const [products] = useProduct();
+
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 place-items-center gap-6 py-5">
-        {products?.map(product=><ProductsCard key={product?._id} item={product}/>)}
+      {products?.map((product, index) => (
+        <motion.div
+          key={product?._id}
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
+          className="w-full"
+        >
+          <ProductsCard item={product} />
+        </motion.div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProductSection
+export default ProductSection;
