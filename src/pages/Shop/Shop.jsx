5e5421ca39d47d8629a320d1b230a92cc2dbe66f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-04 23:20:17 +0600

    feat: implement category navigation and filtering in Shop component

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,5 +1,33 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import useShopItems from "../../Hooks/useShopItems";
 
 const Shop = () => {
+  const { category } = useParams();
+  const navigate = useNavigate();
+  const [shopItems, isLoading,] = useShopItems();
+
+  const activeCategory = category || "popular";
+
+  useEffect(() => {
+    if (!category) {
+      navigate(`/shop/popular`, { replace: true });
+    }
+  }, [category, navigate]);
+
+  const handleTabChange = (newCategory) => {
+    navigate(`/shop/${newCategory}`, { replace: true });
+  };
+
+  const normalizedCategory = activeCategory.toLowerCase();
+
+  // ✅ Filter products based on the updated active category
+  const filteredProducts =
+    activeCategory === "popular"
+      ? shopItems.filter((product) => product.isHotDeal === true)
+      :  shopItems.filter(
+        (product) => product.category.toLowerCase() === normalizedCategory
+      );
   return (
     <div className="flex justify-center items-center text-center min-h-screen text-7xl">Shop</div>
   )
