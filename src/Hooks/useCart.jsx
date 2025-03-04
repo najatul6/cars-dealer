328b390032dffc301d3a8103887c107f56e8c8f0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-04 21:45:24 +0600

    add custom hook for cart management with data fetching

---
+++ src/Hooks/useCart.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
+
+const useCart = () => {
+    const axiosSecure = useAxiosSecure();
+    const {user} = useAuth();
+
+    const {refetch, data: cart = [],isLoading } = useQuery({
+        queryKey: ['cart', user?.email],
+        queryFn: async()=>{
+            const res = await axiosSecure.get(`/carts?email=${user?.email}`)
+            return res.data;
+        }
+    })
+    return [cart,refetch,isLoading]
+};
+
+export default useCart;
